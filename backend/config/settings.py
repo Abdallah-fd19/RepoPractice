cce2fed970b07abb72724c1d07602a88e7feb296
Abdallah-fd19 <abdallah.fya@gmail.com>
2026-03-26 15:12:31 +0300

    fix: stored the secret_key in .env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_p0jlc75kouc%lo9!9bjx7e-w4xylp#lq8ys3ebfk-4_3=uq#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
